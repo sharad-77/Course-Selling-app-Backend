cb837ce6b4131f9ee197a8a1400732df81190129
sharad <138323667+sharad-77@users.noreply.github.com>
2025-02-02 23:34:19 +0530

    user and admin signup and signin completed

--- Router/admin.js
+++ Router/admin.js
@@ -1,31 +1,86 @@
-const { Router } = require('express');
-const {adminSchema} = require('../db')
-const adminRouter =  Router();
+const { Router } = require("express");
+const { adminSchema, adminModel } = require("../db");
+const adminRouter = Router();
 
-adminRouter.post("/signup", async (req,res) => {
+const ADMINSECRET = process.env.JWT_ADMIN_SECRET;
+
+adminRouter.post("/signup", async (req, res) => {
+    const { email, password, firstName, lastName } = req.body;
+    const hasedPass = await bcrypt.hash(password, 5);
+    console.log(hasedPass);
+    try {
+      await adminModel.create({
+        email: email,
+        password: hasedPass,
+        firstName: firstName,
+        lastName: lastName,
+      });
+    } catch {
+      res.status(403).json({
+        message: "Erorr Occured",
+      });
+    } finally {
+      res.json({
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        message: "Done !",
+      });
+      console.log("Signup Complated");
+    }
+  });
+  
+adminRouter.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+  
+    const FindUser = await adminModel.findOne({
+      email
+    });
+  
+    if (!FindUser) {
+      res.json({
+        message: "Your Credential Are Wrong",
+      });
+    }
+    console.log(FindUser);
+  
+    const Comparepass = await bcrypt.compare(password, FindUser.password);
+  
+    if (!Comparepass) {
+      res.json({
+        message: "Your Password Is Wrong",
+      });
+    }
+  
+    const token = jwt.sign({
+      id:FindUser._id
+    },ADMINSECRET);
+  
+    console.log(token)
+  
     res.json({
-        message:"signup endpoint"
-    })
+      token: token,
+      message: "signin endpoint",
+    });
+  });
+
+adminRouter.post("/course", async (req, res) => {
+  res.json({
+    message: "course endpoint",
+  });
+});
+adminRouter.put("/course", async (req, res) => {
+  res.json({
+    message: "course endpoint",
+  });
 });
 
-adminRouter.post("/signin", async (req,res) => {
-    res.json({
-        message:"signin endpoint"
-    })
-});
-
-adminRouter.post("/", async (req,res) => {
-    res.json({
-        message:"course endpoint"
-    })
-});
-
-adminRouter.post("/bulk", async (req,res) => {
-    res.json({
-        message:"course endpoint"
-    })
+adminRouter.get("/course/bulk", async (req, res) => {
+  res.json({
+    message: "course endpoint",
+  });
 });
 
 module.exports = {
-    adminRouter:adminRouter
-}
+  adminRouter: adminRouter,
+};
--- Router/user.js
+++ Router/user.js
@@ -1,25 +1,78 @@
-const {Router} = require("express");
-const {userModel , purchaseModle} = require('../db')
+const { Router } = require("express");
+const bcrypt = require("bcrypt");
+const { configDotenv } = require("dotenv");
+const jwt = require("jsonwebtoken");
+const { userModel, purchaseModle } = require("../db");
 const userRouter = Router();
 
-userRouter.post("/signup", async (req,res) => {
+const USERSECRET = process.env.JWT_USER_SECRET;
+
+userRouter.post("/signup", async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+  const hasedPass = await bcrypt.hash(password, 5);
+  console.log(hasedPass);
+  try {
+    await userModel.create({
+      email: email,
+      password: hasedPass,
+      firstName: firstName,
+      lastName: lastName,
+    });
+  } catch {
+    res.status(403).json({
+      message: "Erorr Occured",
+    });
+  } finally {
     res.json({
-        message:"signup endpoint"
-    })
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+      message: "Done !",
+    });
+    console.log("Signup Complated");
+  }
 });
 
-userRouter.post("/signin", async (req,res) => {
+userRouter.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const FindUser = await userModel.findOne({
+    email
+  });
+
+  if (!FindUser) {
     res.json({
-        message:"signin endpoint"
-    })
+      message: "Your Credential Are Wrong",
+    });
+  }
+  console.log(FindUser);
+
+  const Comparepass = await bcrypt.compare(password, FindUser.password);
+
+  if (!Comparepass) {
+    res.json({
+      message: "Your Password Is Wrong",
+    });
+  }
+
+  const token = jwt.sign({
+    id:FindUser._id
+  },USERSECRET);
+
+  console.log(token)
+
+  res.json({
+    token: token,
+    message: "signin endpoint",
+  });
 });
 
-userRouter.get("/purchases", async (req,res) => {
-    res.json({
-        message:"purchases endpoint"
-    })
+userRouter.get("/purchases", async (req, res) => {
+  res.json({
+    message: "purchases endpoint",
+  });
 });
 
 module.exports = {
-    userRouter:userRouter
-}
+  userRouter: userRouter,
+};
